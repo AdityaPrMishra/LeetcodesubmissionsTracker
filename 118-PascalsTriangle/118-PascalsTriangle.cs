517f1aedf0edbeec1434585cffa12deaa58d347e
AdityaPrMishra <132899121+AdityaPrMishra@users.noreply.github.com>
2026-04-12 15:15:11 +0530

    Create 118-PascalsTriangle

---
+++ 118-PascalsTriangle/118-PascalsTriangle.cs
@@ -0,0 +1,26 @@
+// Last updated: 12/04/2026, 15:15:10
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+     
+        IList<IList<int>> triangle = new List<IList<int>>();
+
+        for (int i=0; i<numRows; i++)
+        {
+            List<int> row = new List<int>();
+
+            for (int j=0; j<=i; j++)
+            {
+                if(j==0||j==i)
+                {
+                   row.Add(1);
+                }
+                else
+                {
+                   row.Add(triangle[i-1][j-1] + triangle[i-1][j]);
+                }
+            }
+            triangle.Add(row);
+        }
+        return triangle;
+    }
+}
